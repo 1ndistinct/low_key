cd44fde685311ab2481e47e19d73f5f9691526f1
PaulMoses87 <PFM2108@users.noreply.github.com>
2020-09-08 16:28:34 +0200

    New Game
    Dynamic Player

---
+++ DynamicUnityProject/Assets/PlayerControlDynamic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlDynamic : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+    public float dashDistance; 
+    public RigidBody2D rb;
+
+// 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
