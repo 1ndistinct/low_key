3fdd0946a9769e0f9c8b59d10bd7e696fdd4a4a4
PaulMoses87 <PFM2108@users.noreply.github.com>
2020-09-24 00:18:14 +0200

    PaullBoss1Update

---
+++ DynamicUnityProject/Assets/BossAttackController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttackController : MonoBehaviour
+{
+    private GameObject PlayerPosition;
+    public Transform TriggerMarker;
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        PlayerPosition = GameObject.Find("Mr.AlienMain");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Anim.SetTrigger("PlayerInRange");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.currentHealth = 0;
+    }
+}
---
+++ DynamicUnityProject/Assets/FlameDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameDamage : MonoBehaviour
+{
+    public float flameDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<PlayerController1>().PlayerTakesDamage(1.0f, 0.5f);
+    }
+}
